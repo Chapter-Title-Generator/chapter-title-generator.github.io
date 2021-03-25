c5c216f443eae9c010a52e3f3fc35212c9e13636
Aiman <65372518+Aiman7756@users.noreply.github.com>
2021-03-24 18:18:42 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,25 +3,13 @@ alert (titles.back);
 function newQuote() {
 	let index = Math.floor(Math.random() * 245)+1;
 	document.getElementById('quoteDisplay').innerHTML = titles[index].body + '<br>';
-
-	//document.getElementById('img').innerHTML = `<img src='${titles[index].filename}' alt='${titles[index].source}'/>`
-	
 	document.getElementById('sourceImage').src = 'logos/' + titles[index].filename;
-
 	document.getElementById('sourceImage').alt = titles[index].source;
-
-	
-
 }
 
-
-
-//"<img src='${filename}' alt=${titles[index].source}/>"
-
-//document.addEventListener('DOMContentLoaded', newQuote() {
-//    alert("Ready!");
-//});
-
+function buttonSound() {
+	document.getElementById("click").play();
+}
 
 
 
