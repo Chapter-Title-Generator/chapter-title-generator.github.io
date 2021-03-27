1043d7084e9f3572f6fc5f44b97e143eb189c5df
toffee7756 <aiman3977@gmail.com>
2021-03-26 19:54:30 -0700

    music and scrolling text

--- javascript.js
+++ javascript.js
@@ -8,8 +8,24 @@ function newQuote() {
 }
 
 function buttonSound() {
+	if (document.getElementById("bg_music").paused)
 	document.getElementById("click").play();
 }
+
+function toggle(){
+	music = document.getElementById("bg_music")
+	if (!music.paused){
+		music.pause();
+		document.getElementById("toggle").src = "buttons/play.png";
+
+	}
+	else {
+		music.play();
+		document.getElementById("toggle").src = "buttons/pause.png";
+	}
+	
+}
+
 
 
 
